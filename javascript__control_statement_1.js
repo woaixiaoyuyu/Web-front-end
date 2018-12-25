2ce10db5919a91d7f5afd5dc89d16a9216cd0cbe
woaixiaoyuyu <38500738+woaixiaoyuyu@users.noreply.github.com>
2018-12-25 23:50:58 +0800

    Create javascript__control_statement_1.js

---
+++ javascript__control_statement_1.js
@@ -0,0 +1,13 @@
+//整理自JavaScript面向对象编程指南（第2版）
+//条件与循环
+1. --if/else
+e.g.
+> result='';
+> somevar = undefined;
+> if (typeof somevar !== "undefined"){
+result = 'yes';
+}
+> result;
+"" 
+
+2. --替代 if 表达式
