3d7f70e79059d0c1815659754e53b96561cab5a5
woaixiaoyuyu <38500738+woaixiaoyuyu@users.noreply.github.com>
2018-12-26 13:30:44 +0800

    Update javascript_function_1.js

--- Javascript/javascript_function_1.js
+++ Javascript/javascript_function_1.js
@@ -1,4 +1,5 @@
 //整理自JavaScript面向对象编程指南（第2版）
+//调用函数；预定义函数
 一、调用函数
 1. --执行任意的参数
 我们还可以创建一些在参数数量方面更为灵活的函数。
@@ -38,3 +39,48 @@ encodeURIComponent()；
 decodeURIComponent()；
 eval()
 */
+1. --parseInt()
+parseInt()会试图将其收到的任何输入值（通常是字符串）转换成整数类型输出。
+如果转换失败就返回 NaN
+> parseInt('231vssdf')
+231
+> parseInt('b231vssdf')
+NaN
+> parseInt('0377', 10);
+377
+> parseInt('0377', 8);
+255 
+> parseInt('0x377');
+887 
+
+2. --parseFloat()
+parseFloat()的功能与 parseInt()基本相同，只不过它仅支持将输入值转换为十进制数。
+因此，该函数只有一个参数
+此外，parseFloat()还可以接受指数形式的数据（这点与 parseInt()不同）
+
+3. --isNaN()
+通过 isNaN()，我们可以确定某个输入值是否是一个可以参与算术运算的数字
+> isNaN(1.23);
+false
+> isNaN(parseInt('abc123'));
+true 
+
+4. --isFinite()
+isFinite()可以用来检查输入是否是一个既非 Infinity 也非 NaN 的数字
+
+5. --encodeURI()/decodeURI()/encodeURIComponent()/decodeURIComponent()
+在 URL（Uniform Resource Locator，统一资源定位符）或 URI（Uniform Resource
+Identifier，统一资源标识符）中，有一些字符是具有特殊含义的。
+如果我们想“转义”这些字符，就可以去调用函数 encodeURI()或 encodeURIComponent()。
+前者会返回一个可用的 URL，而后者则会认为我们所传递的仅仅是 URL 的一部分
+> var url = 'http://www.packtpub.com/scr ipt.php?q=this and that';
+> encodeURI(url);
+"http://www.packtpub.com/scr%20ipt.php?q=this%20and%20that" 
+
+6. --eval()
+eval()会将其输入的字符串当做 JavaScript 代码来执行
+> eval('var ii = 2;');
+> ii;
+2 
+尽管 eval()在某些情况下是很有用的，但如果有选择的话，我们应该尽量避免使用它。
+对于许多经验丰富的 JavaScript 程序员来说，“Eval is evil”（Eval 是魔鬼）是一句至理名言
