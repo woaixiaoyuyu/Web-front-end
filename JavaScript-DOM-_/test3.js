4e3a41ade48f3fd9d05ae7ae53bc2d51ea5c7091
woaixiaoyuyu <38500738+woaixiaoyuyu@users.noreply.github.com>
2019-01-07 15:46:54 +0800

    Delete test3.js

--- JavaScript-DOM-_/test3.js
+++
@@ -1,8 +0,0 @@
-function showPic(whicpic) {
-    var source = whicpic.getAttribute('href');
-    var placeholder = document.getElementById('placeholder');
-    placeholder.setAttribute('src', source);
-    var text = whicpic.getAttribute('title');
-    var description = document.getElementById('description');
-    description.firstChild.nodeValue = text;
-}
