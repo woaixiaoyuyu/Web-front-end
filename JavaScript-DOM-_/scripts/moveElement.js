485afd7acf3bba02ce646e6a576759834297690d
woaixiaoyuyu <38500738+woaixiaoyuyu@users.noreply.github.com>
2019-01-13 16:36:18 +0800

    Add files via upload

--- JavaScript-DOM-_/scripts/moveElement.js
+++ JavaScript-DOM-_/scripts/moveElement.js
@@ -3,15 +3,31 @@ function moveElement(elementID,final_x,final_y,interval) {
     var elem = document.getElementById(elementID);
     var xpos = parseInt(elem.style.left);
     var ypos = parseInt(elem.style.top);
+    if(elem.movement){
+        clearTimeout(elem.movement)
+    }
+    if(!elem.style.left) elem.style.left = '0px';
+    if(!elem.style.top) elem.style.top = '0px';
+    var dist = 0;
     if(xpos===final_x && ypos===final_y) return true;
-    else{
-        if(xpos<final_x) xpos++;
-        if(xpos>final_x) xpos--;
-        if(xpos<final_y) ypos++;
-        if(xpos>final_y) ypos--;
+    if(xpos<final_x){
+        dist = Math.ceil((final_x - xpos)/10);
+        xpos += dist;
+    }
+    if(xpos>final_x){
+        dist = Math.ceil((xpos - final_x)/10);
+        xpos -= dist;
+    }
+    if(ypos<final_y){
+        dist = Math.ceil((final_y - ypos)/10);
+        ypos += dist;
+    }
+    if(ypos>final_y){
+        dist = Math.ceil((ypos - final_y)/10);
+        ypos -= dist;
     }
     elem.style.left = xpos+'px';
     elem.style.top = ypos+'px';
     var repeat = 'moveElement("'+elementID+'",'+final_x+','+final_y+','+interval+')';
-    movement = setTimeout(repeat,interval);
+    elem.movement = setTimeout(repeat,interval);
 }
---
+++ JavaScript-DOM-_/scripts/prepareSlideshow.js
@@ -0,0 +1,28 @@
+function prepareSlideshow() {
+    if(!document.getElementById) return false;
+    if(!document.getElementsByTagName) return false;
+    if(!document.getElementById('linklist')) return false;
+
+    var slideshow = document.createElement('div');
+    slideshow.setAttribute('id','slideshow');
+    var preview = document.createElement('img');
+    preview.setAttribute('src','images/tech3.jpg');
+    preview.setAttribute('id','preview');
+    preview.setAttribute('alt','building blocks of web design');
+    slideshow.appendChild(preview);
+    var list = document.getElementById('linklist');
+    insertAfter(slideshow,list);
+
+    var links = list.getElementsByTagName('a');
+    links[0].onmouseover = function () {
+        moveElement('preview',-100,0,10)
+    }
+    links[1].onmouseover = function () {
+        moveElement('preview',-200,0,10)
+    }
+    links[2].onmouseover = function () {
+        moveElement('preview',-300,0,10)
+    }
+}
+
+addloadEvent(prepareSlideshow);
